e53e732e6a551053610ec8228a47f7c88756e95d
prateekahuja9 <prateekahuja1916@gmail.com>
2025-02-20 15:57:11 -0500

    first commit

---
+++ src/main/java/project/newinventorymanagement1/NewInventoryManagement1Application.java
@@ -0,0 +1,13 @@
+package project.newinventorymanagement1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewInventoryManagement1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewInventoryManagement1Application.class, args);
+    }
+
+}
---
+++ src/test/java/project/newinventorymanagement1/NewInventoryManagement1ApplicationTests.java
@@ -0,0 +1,13 @@
+package project.newinventorymanagement1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewInventoryManagement1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
